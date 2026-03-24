b8145ff285b15f78c902404a858f6ada31f29e26
zakoten <giancarlodematteis98@gmail.com>
2026-03-24 22:31:37 +0100

    Create StonepackPolymerBlockItem.java

---
+++ src/main/java/net/tramonto/stonepack/StonepackPolymerBlockItem.java
@@ -0,0 +1,21 @@
+package net.tramonto.stonepack;
+
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class StonepackPolymerBlockItem extends BlockItem implements PolymerItem {
+
+    public StonepackPolymerBlockItem(Block block, Item.Settings settings) {
+        super(block, settings);
+    }
+
+    @Override
+    public ItemStack getPolymerItemStack(ItemStack itemStack, ServerPlayerEntity player) {
+        return new ItemStack(Items.RED_STAINED_GLASS);
+    }
+}
