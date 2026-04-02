7ac0fd78cd074da8e854bdfd2654ccbebd4a5907
zakoten <giancarlodematteis98@gmail.com>
2026-04-02 14:12:37 +0200

    Update TexturedBlock.java

--- src/main/java/net/tramonto/stonepack/TexturedBlock.java
+++ src/main/java/net/tramonto/stonepack/TexturedBlock.java
@@ -1,21 +1,20 @@
 package net.tramonto.stonepack;
 
 import eu.pb4.polymer.core.api.block.SimplePolymerBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 
 public class TexturedBlock extends SimplePolymerBlock {
-
     private final BlockState clientState;
 
-    public TexturedBlock(Block base, BlockState clientState, BlockBehaviour.Properties settings) {
+    public TexturedBlock(Block base, BlockState clientState, AbstractBlock.Settings settings) {
         super(settings, base);
         this.clientState = clientState;
     }
 
     @Override
     public BlockState getPolymerBlockState(BlockState state) {
-        return clientState;
+        return this.clientState;
     }
 }
